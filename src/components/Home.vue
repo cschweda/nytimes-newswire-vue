165ce8d266bac097c6b64ebdc8a12f7408f127ee
cschweda <cschweda@gmail.com>
2018-01-11 20:53:37 -0600

    Scale cards and add href

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -10,13 +10,8 @@
             <v-flex xs12 sm4 md3 v-for="i in totalCards" :key="i">
                 <v-layout column>
                     <v-flex d-flex>
-                        <!-- <v-card color="blue-grey" dark tile flat>
-                            <v-card-text>
-                                <p v-dummy="50"></p>
-                            </v-card-text>
-                        </v-card> -->
 
-                        <v-card style="margin: 2px;" color="blue-grey darken-3" tile raised>
+                        <v-card :href="getHref(i)" class="post" style="margin: 2px;" color="blue-grey darken-3" tile raised>
                             <v-card-media src="/static/docks.jpg" height="200px">
                             </v-card-media>
                             <v-card-title primary-title>
@@ -30,14 +25,11 @@
                                 </div>
                             </v-card-title>
 
-
                         </v-card>
                     </v-flex>
 
                 </v-layout>
             </v-flex>
-
-
 
         </v-layout>
     </v-container>
@@ -45,57 +37,76 @@
 
 
 <script>
-    import PulseLoader from 'vue-spinner/src/PulseLoader.vue';
-    import { apiKeys } from '@/data/api-keys'
-    import axios from 'axios'
-    export default {
-        mounted() {
-            console.table({ apiKeys })
-            axios.get('/static/data/mock.json')
-                .then(function (response) {
-                    console.log(response);
-                })
-                .catch(function (error) {
-                    console.log(error);
-                });
-        },
-        methods: {
-            showAbstract: function (i) {
-                this.show[i - 1] = !this.show[i - 1]
-                // Find better way?
-                this.$forceUpdate()
-            },
-
-        },
-        components: {
-            PulseLoader
+import PulseLoader from 'vue-spinner/src/PulseLoader.vue';
+// import { apiKeys } from '@/data/api-keys'
+import axios from 'axios'
+export default {
+    mounted() {
+        // console.table({ apiKeys })
+        // axios.get('/static/data/mock.json')
+        //     .then(function(response) {
+        //         console.log(response);
+        //     })
+        //     .catch(function(error) {
+        //         console.log(error);
+        //     });
+    },
+    methods: {
+        showAbstract: function(i) {
+            this.show[i - 1] = !this.show[i - 1]
+            // Find better way?
+            this.$forceUpdate()
         },
 
-        data() {
-            return {
-                show: false,
-                totalCards: 25,
-                show: [],
-                loading: false
-            }
+        getHref: function(x) {
+            return x.toString();
+        }
+
+    },
+    components: {
+        PulseLoader
+    },
+
+    data() {
+        return {
+            show: false,
+            totalCards: 25,
+            show: [],
+            loading: false,
+            test: "1"
         }
     }
+}
 </script>
 
-<style>
-    .nyt {
-        font-family: 'Bevan', cursive;
-    }
+<style scss>
+.nyt {
+    font-family: 'Bevan', cursive;
+}
 
-    .full-height .flex {
-        display: flex
-    }
+.full-height .flex {
+    display: flex
+}
 
-    .full-height .flex>.card {
-        flex: 1 1 auto
-    }
+.full-height .flex>.card {
+    flex: 1 1 auto
+}
 
-    .loading {
-        font-size: 45px;
-    }
+.loading {
+    font-size: 45px;
+}
+
+.post:hover {
+    box-shadow: 0px 0px 150px #000000;
+    z-index: 2;
+    -webkit-transition: all 200ms ease-in;
+    -webkit-transform: scale(1.1);
+    -ms-transition: all 200ms ease-in;
+    -ms-transform: scale(1.1);
+    -moz-transition: all 200ms ease-in;
+    -moz-transform: scale(1.1);
+    transition: all 200ms ease-in;
+    transform: scale(1.1);
+    cursor: pointer;
+}
 </style>
